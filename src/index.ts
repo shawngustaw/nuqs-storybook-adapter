3c5e28ce977c7a2b14c1b7cab4ddc243fb75a90b
Shawn Gustaw <srgustaw@gmail.com>
2026-04-07 11:03:08 -0700

    add nuqs adapter for storybook

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,38 @@
-export function greet(name: string): string {
-	return `Hello, ${name}!`;
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import { useCallback, useRef, useState } from 'react';
+import {
+	type unstable_AdapterOptions as AdapterOptions,
+	unstable_createAdapterProvider as createAdapterProvider,
+	renderQueryString,
+} from 'nuqs/adapters/custom';
+
+function useNuqsStorybookAdapter() {
+	const nextSearchParams = useSearchParams();
+	const initialSearchParams = (
+		nextSearchParams ?? new URLSearchParams()
+	).toString();
+	const locationSearchRef = useRef(initialSearchParams);
+	const [searchParams, setSearchParams] = useState(
+		() => new URLSearchParams(locationSearchRef.current),
+	);
+	const updateUrl = useCallback(
+		(search: URLSearchParams, _options: AdapterOptions) => {
+			const queryString = renderQueryString(search);
+			setSearchParams(new URLSearchParams(search));
+			locationSearchRef.current = queryString;
+		},
+		[],
+	);
+	return {
+		searchParams,
+		updateUrl,
+	};
 }
+
+type NuqsAdapter = ReturnType<typeof createAdapterProvider>;
+
+export const NuqsAdapter: NuqsAdapter = createAdapterProvider(
+	useNuqsStorybookAdapter,
+);
